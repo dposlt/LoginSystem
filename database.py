1a43479467f62675e9ac3120c9cd373beaa8a802
David Poslt <david.poslt@gmail.com>
2020-02-09 19:21:59 +0100

    add definition

--- database.py
+++ database.py
@@ -7,20 +7,25 @@ def conn():
     conn = sqlite3.connect(f'{dbName}')
 
     cursor = conn.cursor()
+    return conn, cursor
+
+def createTable(conn, cursor):
+    conn, cursor = conn()
     tbLogin = 'CREATE TABLE tbLogin (id int, loginName text, loginpass text, date datetime)'
     cursor.execute(f'{tbLogin}')
     conn.commit()
 
+def insertToTable():
     today = datetime.date.today
     tbInsert = f"INSERT INTO tbLogin values ('rowid', 'viperdavid', 'test', '{today})"
     cursor.execute(f'{tbInsert}')
     conn.commit()
 
+def select():
     tbSelect = "Select * from tbLogin"
     s = cursor.execute(f'{tbSelect}')
+    return s
 
-    for i in s:
-        print(i)
     conn.close()
 
 
