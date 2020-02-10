a3d8654f4293dd680deca77dd55d2f4b85eec20f
David Poslt <david.poslt@gmail.com>
2020-02-10 18:39:55 +0100

    add db

--- database.py
+++ database.py
@@ -2,32 +2,43 @@
 import sqlite3
 import datetime
 
-def conn():
+def connect():
     dbName = 'login.db'
     conn = sqlite3.connect(f'{dbName}')
 
     cursor = conn.cursor()
     return conn, cursor
 
-def createTable(conn, cursor):
-    conn, cursor = conn()
+
+def createTable():
+    conn, cursor = connect()
     tbLogin = 'CREATE TABLE tbLogin (id int, loginName text, loginpass text, date datetime)'
     cursor.execute(f'{tbLogin}')
     conn.commit()
 
 def insertToTable():
+    conn, cursor = connect()
     today = datetime.date.today
     tbInsert = f"INSERT INTO tbLogin values ('rowid', 'viperdavid', 'test', '{today})"
     cursor.execute(f'{tbInsert}')
     conn.commit()
 
 def select():
-    tbSelect = "Select * from tbLogin"
+    conn, cursor = connect()
+    tbSelect = "Select * from tbLogin;"
     s = cursor.execute(f'{tbSelect}')
     return s
 
     conn.close()
 
 
+def main():
+    #createTable()
+    insertToTable()
+    conn, cursor = connect()
+    #c = cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    for i in select():
+        print(i)
 
-conn()
+
+main()
