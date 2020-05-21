6fc55ec81ed0841f26d336d864f300567dbcc4ec
dposlt <david.poslt@protonmail.com>
2020-05-21 19:57:18 +0200

    database.py
    add select

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 # sqlite3
 import sqlite3
-from datetime import datetime
+import datetime
 
 def connect():
     dbName = 'login.db'
@@ -12,33 +12,36 @@ def connect():
 
 def createTable():
     conn, cursor = connect()
-    tbLogin = 'CREATE TABLE if not exists tbLogin (id INTEGER PRIMARY KEY AUTOINCREMENT, loginName TEXT NOT NULL UNIQUE, loginpass TEXT NOT NULL, date datetime)'
-    cursor.execute(f'{tbLogin};')
+    tbLogin = 'CREATE TABLE tbLogin (id int, loginName text, loginpass text, date datetime)'
+    cursor.execute(f'{tbLogin}')
     conn.commit()
 
 def insertToTable():
     conn, cursor = connect()
-    today = datetime.now()
-    tbInsert = f"INSERT INTO tbLogin values ('viperdavid2', 'test', '{today}')"
-    cursor.execute(f'{tbInsert};')
+    today = datetime.date.today
+    print(today)
+    exit()
+    tbInsert = f"INSERT INTO tbLogin values ('rowid', 'viperdavid', 'test', '{today})"
+    cursor.execute(f'{tbInsert}')
     conn.commit()
 
 def select():
     conn, cursor = connect()
     tbSelect = "Select * from tbLogin;"
-    s = cursor.execute(f'{tbSelect};')
-    return s
+    s = cursor.execute(f'{tbSelect}')
+    row = s.fetchall()
 
+    return row
     conn.close()
 
 
 def main():
-    createTable()
-    insertToTable()
+    #createTable()
+    #print('execute')
+    #insertToTable()
     conn, cursor = connect()
     #c = cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
     for i in select():
         print(i)
-
 
 main()
